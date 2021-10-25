ca93db8f53aadd258703c8754feb433e9ad1eaa0
AlexHamn <agalexandro16@gmail.com>
2021-10-25 17:02:19 -0500

    raise click event

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -24,11 +24,18 @@ class TodoContainer extends React.Component {
     ]
   };
 
+  handleChange = () => {
+    console.log("clicked");
+  };
+
   render() {
     return (
       <div>
         <Header />
-        <TodosList todos={this.state.todos} />
+        <TodosList
+          todos={this.state.todos}
+          handleChangeProps={this.handleChange}
+        />
       </div>
     )
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,7 +3,11 @@ import React from "react"
 function TodoItem(props) {
     return (
         <li key={props.todo.id}>
-            <input type="checkbox" checked={props.todo.completed}/>
+            <input
+                type="checkbox"
+                checked={props.todo.completed}
+                onChange={() => props.handleChangeProps()}
+            />
             {props.todo.title}
         </li>
     )
