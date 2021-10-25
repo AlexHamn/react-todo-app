045496be564a4ea6107b8c67bb73185c7f6d7d28
AlexHamn <agalexandro16@gmail.com>
2021-10-25 16:42:47 -0500

    add checked state to tasks

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,7 +3,7 @@ import React from "react"
 function TodoItem(props) {
     return (
         <li key={props.todo.id}>
-            <input type="checkbox" />
+            <input type="checkbox" checked={props.todo.completed}/>
             {props.todo.title}
         </li>
     )
