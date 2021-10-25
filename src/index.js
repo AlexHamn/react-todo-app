cee1ade8e1a06adf57eb2a56efe3c266cd7892e6
AlexHamn <agalexandro16@gmail.com>
2021-10-25 14:57:45 -0500

    enable strict mode

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
-
-const element = <h1>Hello from Create React App</h1>
-
-ReactDOM.render(element, document.getElementById("root"))
+//component file
+import TodoContainer from "./components/TodoContainer"
+ReactDOM.render(
+    <React.StrictMode>
+        <TodoContainer />
+    </React.StrictMode>,
+    document.getElementById("root")
+)
