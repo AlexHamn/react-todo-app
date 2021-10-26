f4ea9e62e349f1640063e5dac1383b1e41e4a21c
AlexHamn <agalexandro16@gmail.com>
2021-10-26 10:04:00 -0500

    asign value to text input

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react"
 
 class InputTodo extends Component {
+    state = {
+        title: ""
+    };
   render() {
     return (
       <form>
-        <input type="text" placeholder="Add Todo..." />
+        <input type="text" placeholder="Add Todo..." value={this.state.title}/>
         <button>Submit</button>
       </form>
     )
