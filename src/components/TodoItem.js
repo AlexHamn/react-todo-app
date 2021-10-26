478d560b96152628c845c3415b894c7e963c9994
AlexHamn <agalexandro16@gmail.com>
2021-10-26 11:54:35 -0500

    cross out completed tasks

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,21 +1,32 @@
 import React from "react"
 
+import styles from "./TodoItem.module.css"
+
 function TodoItem(props) {
-    return (
-        <li key={props.todo.id}>
-            <input
-                type="checkbox"
-                checked={props.todo.completed}
-                onChange={() => props.handleChangeProps(props.todo.id)}
-            />
-            <button
-                onClick={() => props.deleteTodoProps(props.todo.id)}
-            >
-                Delete
-            </button>
-            {props.todo.title}
-        </li>
-    )
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#595959",
+    opacity: 0.4,
+    textDecoration: "line-through",
+  }
+  
+  return (
+    <li key={props.todo.id} className={styles.item}>
+      <input
+        type="checkbox"
+        checked={props.todo.completed}
+        onChange={() => props.handleChangeProps(props.todo.id)}
+      />
+      <button
+        onClick={() => props.deleteTodoProps(props.todo.id)}
+      >
+        Delete
+      </button>
+      <span style={props.todo.completed ? completedStyle : null}>
+        {props.todo.title}
+      </span>
+    </li>
+  )
 }
 
 export default TodoItem;
