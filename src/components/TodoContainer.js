592982346ce6cd0a224b879d05072c64e3a41ef3
AlexHamn <agalexandro16@gmail.com>
2021-10-26 10:33:16 -0500

    generate unique ids for every task

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { v4 as uuidv4 } from "uuid";
+
 import TodosList from "./TodosList";
 import Header from "./Header"
 import InputTodo from "./InputTodo"
@@ -8,17 +10,17 @@ class TodoContainer extends React.Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuidv4(),
         title: "Setup development environment",
         completed: true
       },
       {
-        id: 2,
+        id: uuidv4(),
         title: "Develop website and add content",
         completed: false
       },
       {
-        id: 3,
+        id: uuidv4(),
         title: "Deploy to live server",
         completed: false
       }
@@ -49,7 +51,7 @@ class TodoContainer extends React.Component {
 
   addTodoItem = title => {
     const newTodo = {
-      id: 4,
+      id: uuidv4(),
       title: title,
       completed: false
     };
