e38f607de998f3fc90eabf0b32d651986a700d4f
AlexHamn <agalexandro16@gmail.com>
2021-10-26 16:27:44 -0500

    modify input code

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -1,17 +1,24 @@
 import React, { useState } from "react"
 
 const InputTodo = props => {
-  const [title, setTitle] = useState("")
+  const [inputText, setInputText] = useState({
+    title: "",
+  })
 
   const onChange = e => {
-    setTitle(e.target.value)
+    setInputText({
+      ...inputText,
+      [e.target.name]: e.target.value,
+    })
   }
 
   const handleSubmit = e => {
     e.preventDefault()
-    if (title.trim()) {
-      props.addTodoProps(title)
-      setTitle("")
+    if (inputText.title.trim()) {
+      props.addTodoProps(inputText.title)
+      setInputText({
+        title: "",
+      })
     } else {
       alert("Please write item")
     }
@@ -23,7 +30,7 @@ const InputTodo = props => {
         type="text"
         className="input-text"
         placeholder="Add todo..."
-        value={title}
+        value={inputText.title}
         name="title"
         onChange={onChange}
       />
